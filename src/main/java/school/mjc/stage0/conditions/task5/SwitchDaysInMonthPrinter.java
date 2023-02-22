0da053dc4f2a8df2cd7a5f8f7ad0944e0b1c3a9b
oidagsdhsagdhas44 <tornike.buchukuri@gau.edu.ge>
2023-02-23 01:03:38 +0400

    module4 task5

--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -2,5 +2,24 @@ package school.mjc.stage0.conditions.task5;
 
 public class SwitchDaysInMonthPrinter {
     public void amountOfDays(int month) {
+        switch(month){
+            case 2:
+                System.out.println(28);
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 9:
+            case 11:
+            case 12:
+                System.out.println(31);
+            case 4:
+            case 6:
+            case 8:
+            case 10:
+                System.out.println(31);
+            default:
+                System.out.println("wrong number!");
+        }
     }
 }
